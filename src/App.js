e52ffd1a27dd3cae176b29cce26693cc308cecc7
Patricia Montoya <patmonhin@gmail.com>
2019-06-26 12:10:09 -0500

    Styling basics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Heading from './components/Heading'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ color: "red" }}>I'm inline style</h1>
+      <h2 className="second">I'm App css style</h2>
+      <h3 className="property">I'm from css variables</h3>
+      <Heading />
     </div>
   );
 }
---
+++ src/components/Heading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Heading.css'
+
+export default function Heading() {
+    return (
+        <h1 className="third">
+            I'm a separate css file 
+        </h1>
+    )
+}
