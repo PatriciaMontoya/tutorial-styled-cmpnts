bb60d8a23baf7e2cbcbcd93bee9d79a72223c1a2
Patricia Montoya <patmonhin@gmail.com>
2019-07-02 11:41:11 -0500

    Extending styles

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import React from 'react';
-import './App.css';
-import { ButtonWrapper } from './components/Button'
-import mainBcg from './images/mainBcg.jpg'
-import secondaryBcg from './images/secondaryBcg.jpg'
-import { Header } from './components/Header'
-import { Hero } from './components/Header'
-import Banner from './components/Banner'
-import { createGlobalStyle } from 'styled-components'
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-
-  body {
-    font-family: Verdana, Geneva, Tahoma, sans-serif;
-  }
-`
+import React from 'react'
+import { DangerButton, SuccessButton } from './components/Buttons'
 
 function App() {
   return (
     <>
-    <GlobalStyle/>
-      <Header img={mainBcg}>
-        <Banner title="Fresh coffee is the best">
-            <ButtonWrapper>Click me!!</ButtonWrapper>
-        </Banner>
-      </Header>
-      <Hero img={secondaryBcg}/>
+      <DangerButton>Danger</DangerButton>
+      <SuccessButton>Success</SuccessButton>
     </>
   );
 }
