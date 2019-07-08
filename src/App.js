060d28ab297d49e323a1cfef882cad7d3adca553
Patricia Montoya <patmonhin@gmail.com>
2019-07-08 11:54:05 -0500

    using animations

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import { RedBox, GreenBox } from './components/Boxes'
+import { Box } from './components/AnimatedBox'
 
 class App extends React.Component {
 
   render() {
     return (
       <div>
-        <RedBox color="red"/>
-        <GreenBox color="green"/>
+       <Box />
       </div>
     );
   }
